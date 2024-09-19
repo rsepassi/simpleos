de81952d7c838a829eb3d3afa24cd37753196873
Ryan Sepassi <rsepassi@gmail.com>
2024-09-19 13:32:16 -0700

    FAT16 boot partition to make the image 8MB

--- do/build_boot.sh
+++ do/build_boot.sh
@@ -16,9 +16,9 @@ BIOS=$BUILD/limine/build/share/limine/limine-bios.sys
 LIMINE=$BUILD/limine/build/bin/limine
 LIMINE_CFG=$ROOT/src/limine.conf
 
-# Create and populate the FAT32 filesystem
-dd if=/dev/zero of=$FAT bs=62M count=1
-mformat -F -i $FAT ::
+# Create and populate the FAT16 filesystem
+dd if=/dev/zero of=$FAT bs=6M count=1
+mformat -i $FAT ::
 mmd -i $FAT ::/boot ::/EFI ::/EFI/BOOT
 mcopy -i $FAT $EFI ::/EFI/BOOT/$LIMINE_EFI_BIN
 mcopy -i $FAT $BIOS ::/boot/limine-bios.sys
@@ -26,14 +26,14 @@ mcopy -i $FAT $LIMINE_CFG ::/boot/limine.conf
 mcopy -i $FAT $KERNEL ::/boot/kernel
 
 # Make the GPT container
-dd if=/dev/zero of=$IMG bs=64M count=1
+dd if=/dev/zero of=$IMG bs=8M count=1
 parted -s $IMG -- \
   unit MiB \
   mklabel gpt \
-  mkpart primary fat32 1MiB 63MiB \
+  mkpart primary fat32 1MiB 7MiB \
   set 1 esp on
 
-# Copy in the FAT32 partition
+# Copy in the FAT16 partition
 dd if=$FAT of=$IMG bs=1M seek=1 conv=notrunc
 
 # Install Limine BIOS
